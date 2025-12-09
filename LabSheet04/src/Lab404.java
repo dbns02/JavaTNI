9dd5043d8d535bec3f261ad2079be3b3ad8c589d
dbns02 <nattinan081149@gmail.com>
2025-12-09 13:52:34 +0700

    LabSheet

--- LabSheet04/src/Lab404.java
+++ LabSheet04/src/Lab404.java
@@ -5,15 +5,17 @@ public class Lab404 {
         Scanner scan = new Scanner(System.in);
 
         String word;
+        String sum = "";
 
         while (true) {
-            System.out.println("Enter word: ");
+            System.out.print("Enter word: ");
             word = scan.nextLine();
 
             if (word.equalsIgnoreCase("stop")) {
                 break;
             }
+            sum = sum + word + " ";
         }
-
+        System.out.println(sum.trim());
     }
 }
--- LabSheet04/src/Lab405.java
+++ LabSheet04/src/Lab405.java
@@ -4,7 +4,25 @@ public class Lab405 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("");
+        String sentence;
 
+        System.out.print("Input some sentence: ");
+        sentence = scan.nextLine();
+
+        while (true) {
+            if (sentence.endsWith(".")) {
+                break;
+            }
+
+            System.out.print("The sentence must end with stop point: ");
+            sentence = scan.nextLine();
+        }
+        sentence = sentence.substring(0, sentence.length() - 1);
+
+        String[] words = sentence.split(" ");
+
+        for (String w : words) {
+            System.out.println(w);
+        }
     }
 }
