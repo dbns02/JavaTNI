14ffe30f626a0dbb5f44ea19329e7a59862770b3
dbns02 <nattinan081149@gmail.com>
2025-12-16 12:25:18 +0700

    LabSheet05

--- LabSheet05/src/Lab503.java
+++ LabSheet05/src/Lab503.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class Lab503 {
+
     public static int count_vowel(String message, char letter) {
-        
+        int count = 0;
+
+        message = message.toLowerCase();
+        letter = Character.toLowerCase(letter);
+
+        if ("a e i o u".indexOf(letter) == -1) {
+            return 0;
+        }
+
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) == letter) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a message: ");
+        String msg = sc.nextLine();
+
+        System.out.println("List of vowel in \"" + msg + "\"");
+
+        System.out.println("The letter 'A' appears " + count_vowel(msg, 'A') + " time(s).");
+        System.out.println("The letter 'E' appears " + count_vowel(msg, 'E') + " time(s).");
+        System.out.println("The letter 'I' appears " + count_vowel(msg, 'I') + " time(s).");
+        System.out.println("The letter 'O' appears " + count_vowel(msg, 'O') + " time(s).");
+        System.out.println("The letter 'U' appears " + count_vowel(msg, 'U') + " time(s).");
     }
 }
--- LabSheet05/src/Lab505.java
+++ LabSheet05/src/Lab505.java
@@ -1,2 +1,41 @@
+import javax.swing.*;
+
 public class Lab505 {
+
+    public static boolean is_palindrome(String word) {
+
+        word = word.toLowerCase(); // ไม่สนใจตัวพิมพ์เล็ก-ใหญ่
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String word = JOptionPane.showInputDialog(
+                null,
+                "Enter some word:",
+                "Palindrome",
+                JOptionPane.QUESTION_MESSAGE
+        );
+
+        if (is_palindrome(word)) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    word + " is Palindrome");
+        } else {
+            JOptionPane.showMessageDialog(
+                    null,
+                    word + " is not Palindrome"
+            );
+        }
+    }
 }
